2aa87068a1ca4da3c98fcf0edfe9217077dff4c1
wrd11 <will.derenzymartin@gmail.com>
2014-01-23 17:56:41 +0000

    added basic haskell ping server

---
+++ servers/basic_ping.hs
@@ -0,0 +1,22 @@
+import Network
+import Control.Concurrent
+import System.IO
+import System.Environment
+
+
+main =  withSocketsDo $ do
+        sock <- listenOn $ PortNumber 5002 
+        loop sock
+
+loop sock = do
+    (h,_,_) <- accept sock
+    forkIO $ body h
+    loop sock
+    where
+        body h = do
+            hPutStr h msg
+            hFlush h
+            hClose h
+
+msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
+
