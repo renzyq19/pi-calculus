c277a95caa4f83f459a994659521ed734f5d6bed
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2014-04-21 12:40:26 +0100

    first complete parser

--- parser/PiParser.hs
+++ parser/PiParser.hs
@@ -1,32 +1,143 @@
 module PiParser where
 
+import Control.Monad (liftM)
 import PiProcess
 import Text.ParserCombinators.Parsec
 
 
 parseIn :: Parser PiProcess
 parseIn = do
-           string "in("
-           name <- readName
-           char ','
-           var  <- readVar
-           char ')'
-           return $ In name var 
+            string "in("
+            name <- readName
+            paddedComma
+            var  <- readVar
+            char ')'
+            return $ In name var 
 
 parseOut :: Parser PiProcess
 parseOut = do
-           string "out("
-           name <- readName
-           char ','
-           term  <- readTerm
-           char ')'
-           return $ Out name term 
+            string "out("
+            name <- readName
+            paddedComma
+            term  <- parseTerm
+            char ')'
+            return $ Out name term 
 
-parseProcess :: Parser PiProcess
-parseProcess = undefined
 parseReplicate :: Parser PiProcess
 parseReplicate = do
             string "!("
-            process <- 
+            process <- parseProcess
+            char ')'
+            return $ Replicate process
 
+parseConc :: Parser PiProcess
+parseConc = do
+            p1 <- parseProcess 
+            spaces
+            char '|'
+            spaces
+            p2 <- parseProcess
+            return $ p1 `Conc` p2
+
+parseSeq :: Parser PiProcess
+parseSeq = do
+            p1 <- parseProcess 
+            spaces
+            char ';'
+            spaces
+            p2 <- parseProcess
+            return $ p1 `Seq` p2
+
+parseNew :: Parser PiProcess
+parseNew = do
+            string "new"
+            spaces
+            name <- readName
+            return $ New name
+
+parseIf :: Parser PiProcess
+parseIf = do
+            string "if" 
+            spaces
+            cond <- parseCondition
+            spaces
+            string "then"
+            spaces
+            p1 <- parseProcess
+            spaces
+            string "else"
+            spaces
+            p2 <- parseProcess
+            return $ If cond p1 p2
+
+parseLet :: Parser PiProcess
+parseLet = do
+            string "let"
+            spaces
+            name <- readName
+            spaces
+            char '='
+            spaces
+            term <- parseTerm
+            spaces
+            string "in"
+            spaces
+            p   <- parseProcess
+            return $ Let name term p
+
+parseCondition :: Parser Condition
+parseCondition = do
+            t1 <- parseTerm
+            spaces
+            string "=="
+            spaces
+            t2 <- parseTerm
+            return $ t1 `Equals` t2
+
+parseTName :: Parser Term
+parseTName = liftM TName readName
+
+parseTVar :: Parser Term
+parseTVar = parseTName 
+
+parseTFun :: Parser Term
+parseTFun = do
+            name <- readName
+            spaces
+            char '('
+            spaces
+            args <- sepBy parseTerm paddedComma
+            spaces
+            char ')'
+            return $ TFun name args (length args)
+
+readName :: Parser Name
+readName = do
+            first <- letter
+            rest <- many $ letter <|> digit
+            return $ first:rest
+
+paddedComma :: Parser ()
+paddedComma = do
+                spaces
+                char ','
+                spaces
+
+readVar :: Parser Variable
+readVar = readName 
+
+parseTerm :: Parser Term
+parseTerm =  parseTName
+         <|> parseTVar
+         <|> parseTFun
+
+parseProcess :: Parser PiProcess
+parseProcess =  parseIn 
+            <|> parseOut
+            <|> parseNew
+            <|> parseSeq
+            <|> parseConc
+            <|> parseReplicate
+            <|> parseLet
+            <|> parseIf
 
--- parser/PiProcess.hs
+++ parser/PiProcess.hs
@@ -4,7 +4,7 @@ import Data.List (intercalate)
 
 data PiProcess = In   Name Variable
                | Out  Name Term
-               | New Name
+               | New  Name
                | PiProcess `Seq`   PiProcess -- Sequential Composition
                | PiProcess `Conc`  PiProcess -- Parallel   Composition
                | Replicate PiProcess         -- Infinite parallel replication
@@ -19,7 +19,7 @@ data Term = TName Name
 
 type Variable   = String
 type Name      = String
-data Condition = Term `Equals` Term deriving (Eq, Show)
+data Condition = Term `Equals` Term deriving (Eq)
 
 showPi :: PiProcess -> String
 showPi (In c m) =  "in(" ++ c ++ "," ++ m ++ ")"
@@ -29,7 +29,7 @@ showPi (p1 `Conc` p2) = show p1 ++ "|\n" ++ show p2
 showPi (p1 `Seq` p2) = show p1 ++ ";\n" ++ show p2 
 showPi (New n)   = "new " ++ n
 showPi (If c p1 p2) = "if " ++ show c ++ " then " ++ show p1 ++ " else " ++ show p2
-showPi (Let n t p2) = "let " ++ n ++ " = " ++ show t ++ " in\n" ++ show p2
+showPi (Let n t p) = "let " ++ n ++ " = " ++ show t ++ " in\n" ++ show p
 
 showTerm :: Term -> String
 showTerm (TVar x) = x
@@ -37,5 +37,10 @@ showTerm (TName n)= n
 showTerm (TFun n [] 0) = n
 showTerm (TFun n ts _) = n ++ "(" ++ intercalate "," (map show ts) ++ ")"
 
+showCond :: Condition -> String
+showCond (t1 `Equals` t2) = show t1 ++ " == " ++ show t2
+
+
 instance Show PiProcess where show = showPi
 instance Show Term where show = showTerm
+instance Show Condition where show = showCond
