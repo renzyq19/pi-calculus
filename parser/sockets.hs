be6ef7a225cc199a9ab691e7e51d142733a71de1
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2014-05-15 19:59:03 +0100

    still more networking to do

--- parser/sockets.hs
+++ parser/sockets.hs
@@ -4,11 +4,22 @@ import Control.Concurrent
 
 main :: IO ()
 main = withSocketsDo $ do
-    forkIO $ do s1 <- connectTo "localhost" $ PortNumber 9000
-                hPutStrLn s1 "hello"
+    _ <- forkIO recv
+    --_ <- forkIO send
+    return ()
+
+recv :: IO ()
+recv = do
     s2 <- listenOn $ PortNumber 9000
     (h,_,_) <- accept s2
     str <- hGetLine h
+    hShow h >>= putStrLn
+    sClose s2
     putStrLn str
+
+send :: IO ()
+send = do 
+    s1 <- connectTo "localhost" $ PortNumber 9000
+    hPutStrLn s1 "hello"
 
 
